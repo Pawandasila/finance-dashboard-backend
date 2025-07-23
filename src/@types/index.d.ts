0b7b61166c7c0dfbcf6ccc0f06dad09e617cad0f
Pawandasila <pawandasila06@gmail.com>
2025-07-23 14:41:43 +0530

    fixed types: :

---
+++ src/@types/index.d.ts
@@ -0,0 +1,10 @@
+import { UserDocument } from "../models/user.models";
+
+declare global {
+    namespace Express {
+        interface User extends UserDocument {
+            _id: any;
+            id : string;
+        }
+    }
+}
